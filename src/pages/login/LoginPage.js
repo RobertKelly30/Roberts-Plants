bb4d833e3f9590849e17d2133e274c31aa6b34b7
rkelly30 <46577129+rkelly30@users.noreply.github.com>
2022-03-11 14:26:48 -0700

    Created Styles for Login Page

--- src/pages/login/LoginPage.js
+++ src/pages/login/LoginPage.js
@@ -1,26 +1,55 @@
 import React from "react";
-import { useNavigate } from 'react-router-dom'
+import { useNavigate } from "react-router-dom";
 
-function LoginPage (props){
-    let navigation = useNavigate();
+import { Label, Input } from "./../../ui/forms";
 
-    function onHandleSubmit(e){
-        e.preventDefault();
-        navigation('/dashboard');
-    }
+import { SubmitButton } from "../../ui/buttons";
+import { LoginPageStyles, FormControl } from "./styles";
 
-    return(
-        <>
-            <header>
-                <h1>Login Page</h1>
-            </header>
+function LoginPage(props) {
+  let navigation = useNavigate();
 
-            <form onSubmit={onHandleSubmit}>
-                <input type="text" required />
-                <button type="submit">Sign In</button>
-            </form>
-        </>
-    )
+  function onHandleSubmit(e) {
+    e.preventDefault();
+    navigation("/dashboard");
+  }
+
+  return (
+    <>
+      <LoginPageStyles>
+        <header>
+          <h1>Login Page</h1>
+        </header>
+
+        <form onSubmit={onHandleSubmit}>
+          <FormControl className="form-control">
+            <Label htmlFor="email">Email</Label>
+            <Input id="email" type="text" placeholder="johndoe@gmail.com" />
+          </FormControl>
+
+          <FormControl className="form-control">
+            <Label htmlFor="password">Password</Label>
+            <Input id="password" type="text" placeholder="Password" />
+          </FormControl>
+
+          <FormControl className="form-control">
+            <SubmitButton
+              type="submit"
+              padding="0.88rem"
+              margin="1rem 0 0"
+              fs="1rem"
+            >
+              Sign In
+            </SubmitButton>
+          </FormControl>
+        </form>
+      </LoginPageStyles>
+      <LoginPageStyles>
+        <h2>Welcome to Robert's Plants</h2>
+        <p>Enjoy the greenery</p>
+      </LoginPageStyles>
+    </>
+  );
 }
 
-export default LoginPage
+export default LoginPage;
